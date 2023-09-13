70be28f5d06f0cf33a1275dd0ac6503478147f8e
ruslan <mavitov98@icloud.com>
2023-09-13 10:22:15 +0300

    Демо контроллер

---
+++ src/Controller/DemoController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class DemoController extends AbstractController
+{
+    #[Route(path: '/demo', methods: ['POST'])]
+    public function demo(): Response
+    {
+        return $this->json(['message' => 'Hello!']);
+    }
+}
