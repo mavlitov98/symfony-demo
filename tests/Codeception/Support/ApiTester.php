a4e6965c22639f5121d1fa101009e8e1d9feadf5
ruslan <mavitov98@icloud.com>
2023-09-14 15:32:06 +0300

    Установлен и настроен Codeception

---
+++ tests/Codeception/Support/ApiTester.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Codeception\Support;
+
+/**
+ * Inherited Methods
+ * @method void wantTo($text)
+ * @method void wantToTest($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause($vars = [])
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class ApiTester extends \Codeception\Actor
+{
+    use \App\Tests\Codeception\Support\_generated\ApiTesterActions;
+}
