c217298e4d39b3f38093c862a8d17c69a37e14d6
ruslan <mavitov98@icloud.com>
2023-09-14 15:32:16 +0300

    Первый API тест

---
+++ tests/Api/Demo/DemoCest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Api\Demo;
+
+use App\Tests\Codeception\Support\ApiTester;
+
+class DemoCest
+{    
+    public function demo(ApiTester $I): void
+    {
+        $I->sendPost('/demo');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseContainsJson(['message' => 'Hello from Demo!']);
+    }
+}
